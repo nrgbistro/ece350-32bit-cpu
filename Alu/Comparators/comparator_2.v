32f57c7e72739482c169c03c2ba86fab1feabd22
nolan gelinas <nrg23@duke.edu>
2023-02-05 12:47:43 -0500

    Fix implicit wire definition

--- Alu/Comparators/comparator_2.v
+++ Alu/Comparators/comparator_2.v
@@ -4,20 +4,20 @@ module comparator_2(
     input [1:0] a, b);
 
     wire eq0, eq1, eq2, lt0, lt1, lt2, lt3, lt4;
-    // wire notA1, notA0, notB1, notB0;
+    wire notA1Wire, notA0Wire, notB1Wire, notB0Wire;
 
-    not notA1(notA1, a[1]);
-    not notA0(notA0, a[0]);
-    not notB1(notB1, b[1]);
-    not notB0(notB0, b[0]);
+    not notA1(notA1Wire, a[1]);
+    not notA0(notA0Wire, a[0]);
+    not notB1(notB1Wire, b[1]);
+    not notB0(notB0Wire, b[0]);
 
     xnor xnor0(eq0, a[0], b[0]);
     xnor xnor1(eq1, a[1], b[1]);
     and EQAnd(EQ, eq0, eq1, EQ_in);
 
-    and and0(lt0, notA1, notA0, notB1, b[0]);
-    and and1(lt1, notA1, b[1]);
-    and and2(lt2, a[1], notA0, b[1], b[0]);
+    and and0(lt0, notA1Wire, notA0Wire, notB1Wire, b[0]);
+    and and1(lt1, notA1Wire, b[1]);
+    and and2(lt2, a[1], notA0Wire, b[1], b[0]);
     or LTOr(lt3, lt0, lt1, lt2);
     and LTAnd(lt4, lt3, EQ_in);
     or LTOr2(LT, lt4, LT_in);
--- Alu/Comparators/comparator_32.v
+++ Alu/Comparators/comparator_32.v
@@ -2,16 +2,21 @@ module comparator_32(
     output LT, EQ,
     input [31:0] a, b);
 
-    wire LT3, EQ3, LT2, EQ2, LT1, EQ1, LT_in, EQ_in;
+    wire LT3, EQ3, LT2, EQ2, LT1, EQ1, LT_in, LT_chain_output, EQ_in, a32InverseWire, b32InverseWire, forceGTWire;
 
-    not b32Inverse(b32Inverse, b[31]);
-
-    and twosCompCheck(LT_in, a[31], b32Inverse);
+    not a32Inverse(a32InverseWire, a[31]);
+    not b32Inverse(b32InverseWire, b[31]);
+    and forceLT(LT_in, a[31], b32InverseWire);
+    and forceGT(forceGTWire, b[31], a32InverseWire);
     assign EQ_in = 1;
 
     comparator_8 comp3(LT3, EQ3, LT_in, EQ_in, a[31:24], b[31:24]);
     comparator_8 comp2(LT2, EQ2, LT3, EQ3, a[23:16], b[23:16]);
     comparator_8 comp1(LT1, EQ1, LT2, EQ2, a[15:8], b[15:8]);
-    comparator_8 comp0(LT, EQ, LT1, EQ1, a[7:0], b[7:0]);
+    comparator_8 comp0(LT_chain_output, EQ, LT1, EQ1, a[7:0], b[7:0]);
+
+    mux_2_1bit mux(LT, forceGTWire, LT_chain_output, 1'b0);
+
+
 
 endmodule
