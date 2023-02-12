4a81e6b6deaecc204bab0c700c5d947cc59f2ead
nrgbistro <nolangelinas@gmail.com>
2023-02-12 15:03:49 -0500

    Add multiplicand shifting

--- Mult/mult.v
+++ Mult/mult.v
@@ -4,10 +4,10 @@ module mult(
     input enable, clk, count0bool, rst);
 
     wire [64:0] product_out, product_in, initialProduct;
-    wire [31:0] adderResult;
+    wire [31:0] adderResult, multiplicandShifterResult, regMultiplicandOut;
     wire [2:0] multOpCode;
     wire [1:0] productInputSelectWire;
-    wire subCode, adderOverflow;
+    wire subCode, adderOverflow, shiftMultiplicand;
 
     assign ans = product_out[32:1];
     assign opCode[1:0] = product_out[1:0];
@@ -21,12 +21,13 @@ module mult(
 
     assign multOpCode[2:0] = product_out[2:0];
 
-    multControl controller(productInputSelectWire, subCode, multOpCode, count0bool, enable);
+    multControl controller(productInputSelectWire, subCode, shiftMultiplicand, multOpCode, count0bool, enable);
 
-    //
     mux4_65 regProductInputSelector(product_in, productInputSelectWire, product_out >>> 2, adder_result >>> 2, initialProduct, initialProduct);
 
+    mux2 regMultiplicandShiftSelector(multiplicandShifterResult, shiftMultiplicand, multiplicand, multiplicand <<< 1);
+
     register65 regProduct( .out(product_out), .data(product_in), .clock(clk), .enable(enable), .reset(rst) );
-    register32 regMultiplicand(.out(b), .data(b), .clock(clk), .enable(count0bool), .reset(rst));
+    register32 regMultiplicand(.out(regMultiplicandOut), .data(productInputSelectWire), .clock(clk), .enable(count0bool), .reset(rst));
 
 endmodule
--- Mult/multControl.v
+++ Mult/multControl.v
@@ -2,7 +2,7 @@ module multControl(
     output [1:0] productInputCode,
     output sub, shiftMultiplicand,
     input [2:0] opCode,
-    input count0bool, enable);
+    input count0bool, multEnabled);
 
     wire chooseAdd;
 
@@ -10,5 +10,6 @@ module multControl(
     assign productInputCode[1] = count0bool;
 
     assign shiftMultiplicand = (~opCode[0] & opCode[1] & opCode[2]) | (opCode[0] & ~opCode[1] & ~opCode[2]) ? 1'b1 : 1'b0;
+
 
 endmodule
