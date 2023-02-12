1bfa62ebb78dd525e61d5f34832190265358e094
nolan gelinas <nrg23@duke.edu>
2023-02-12 13:44:42 -0500

    Connect controller

--- Mult/mult.v
+++ Mult/mult.v
@@ -1,9 +1,9 @@
 module mult(
     output [31:0] ans,
-    input [31:0] a, b,
+    input [31:0] multiplicand, multiplier,
     input enable, clk, count0bool, rst);
 
-    wire [65:0] product_out, product_in;
+    wire [65:0] product_out, product_in, initialProduct;
     wire [2:0] multOpCode;
     wire [1:0] productInputSelectWire;
     wire subCode;
@@ -12,9 +12,13 @@ module mult(
     assign opCode[1:0] = product_out[1:0];
     assign opCode[2] = product_out[2];
 
-    multControl controller(productInputSelectWire, subCode, multOpCode, count0bool);
+    assign initialProduct[1:0] = 2'b0;
+    assign initialProduct[33:2] = multiplier;
+    assign initialProduct[65:34] = 32'b0;
 
-    mux4_1 regProductInputSelector(product_in, productInputSelectWire, product_out >>> 1, 0, 0, 0);
+    multControl controller(productInputSelectWire, subCode, multOpCode, count0bool, enable);
+
+    mux4_1 regProductInputSelector(product_in, productInputSelectWire, product_out >>> 1, 0, initialProduct, initialProduct);
 
     register66 regProduct( .out(product_out), .data(product_in), .clock(clk), .enable(enable), .reset(rst) );
     register32 regMultiplicand(.out(b), .data(b), .clock(clk), .enable(count0bool), .reset(rst));
--- Mult/multControl.v
+++ Mult/multControl.v
@@ -2,6 +2,11 @@ module multControl(
     output [1:0] productInputCode,
     output sub,
     input [2:0] opCode,
-    input count0bool);
+    input count0bool, enable);
+
+    wire chooseAdd;
+
+    assign productInputCode[0] = chooseAdd;
+    assign productInputCode[1] = count0bool;
 
 endmodule
