b01dd221a35fd4399545c5c6f868023d0477763f
nolan gelinas <nrg23@duke.edu>
2023-03-19 17:00:02 -0400

    fix branch and set

--- Processor/control/PCControl.v
+++ Processor/control/PCControl.v
@@ -11,7 +11,7 @@ module PCControl(
     adder_32 adder(executePCPlusImmediate, adderOverflow, executePC, executeImmediate, 1'b0);
 
     assign nextPC = branch ? branchAddr : PCPlusOne;
-    assign branchAddr = (executeOpcode == 5'b00001) || (executeOpcode == 5'b00011) ? executeT :
+    assign branchAddr = (executeOpcode == 5'b00001) || (executeOpcode == 5'b00011) || (executeOpcode == 5'b10110 && neq) ? executeT :
                         (executeOpcode == 5'b00010 && neq) || (executeOpcode == 5'b00110 && lt) ? executePCPlusImmediate :
                         (executeOpcode == 5'b00100) ? executeA :
                         {32{1'bz}};
--- build_testbench.bat
+++ build_testbench.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 cmd /c "update_deps.bat"
-cmd /c "iverilog -o proc.out -c deps.f -s Wrapper_tb -P Wrapper_tb.FILE=\""bypass_branch\"""
+cmd /c "iverilog -o proc.out -c deps.f -s Wrapper_tb -P Wrapper_tb.FILE=\""branch_bex\"""
 cmd /c "move proc.out Tests/"
