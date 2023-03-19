93349d695103ca654b96ec9cc7923dcb42c7cac0
nolan gelinas <nrg23@duke.edu>
2023-03-19 17:56:09 -0400

    Fix bypass from memory

--- Processor/Hazard/bypass.v
+++ Processor/Hazard/bypass.v
@@ -3,14 +3,14 @@ module Bypass(
     output dmem_bypass,
     input [31:0] executeIR, memoryIR, writebackIR);
 
-    wire [4:0] executeRS1, executeRS2, memoryRD, writebackRD, executeOpcode, memoryOpcode, writebackOpcode;
+    wire [4:0] executeRD, executeRS1, executeRS2, memoryRD, writebackRD, executeOpcode, memoryOpcode, writebackOpcode;
     wire altInstruction;
 
     // Change which registers are used for detection when the instruction is a branch
     assign altInstruction = executeOpcode == 5'b00010 ||
                             executeOpcode == 5'b00110 ||
                             executeOpcode == 5'b00100;
-
+    assign executeRD = executeIR[26:22];
     assign executeRS1 = altInstruction ? executeIR[26:22] : executeIR[21:17];
     assign executeRS2 = altInstruction ? executeIR[21:17] : executeIR[16:12];
     assign memoryRD = memoryIR[26:22];
@@ -25,8 +25,8 @@ module Bypass(
     // 2'b1x: No bypass
 
     // ALU A
-    assign ALU_A_bypass = (executeRS1 != 5'b0 && executeRS1 == memoryRD && executeOpcode != 5'b00111) ? 2'b00 :
-                          (executeRS1 != 5'b0 && executeRS1 == writebackRD && executeOpcode != 5'b00111) ? 2'b01 : 2'b10;
+    assign ALU_A_bypass = (executeRS1 != 5'b0 && (executeRS1 == writebackRD || (executeOpcode == 5'b00111 && executeRD == writebackRD))) ? 2'b01 :
+                          (executeRS1 != 5'b0 && (executeRS1 == memoryRD || (executeOpcode == 5'b00111 && executeRD == memoryRD))) ? 2'b00 : 2'b10;
 
     // ALU B
     assign ALU_B_bypass = (executeRS2 != 5'b0 && executeRS2 == writebackRD) ? 2'b01 :
