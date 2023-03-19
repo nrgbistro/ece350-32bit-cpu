fd1b5d9a3a6d5645ac2bfdfa0467d7c5bbdb586c
nolan gelinas <nrg23@duke.edu>
2023-03-19 16:51:48 -0400

    Fix bypass error

--- build_testbench.bat
+++ build_testbench.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 cmd /c "update_deps.bat"
-cmd /c "iverilog -o proc.out -c deps.f -s Wrapper_tb -P Wrapper_tb.FILE=\""bypass_bex\"""
+cmd /c "iverilog -o proc.out -c deps.f -s Wrapper_tb -P Wrapper_tb.FILE=\""bypass_branch\"""
 cmd /c "move proc.out Tests/"
