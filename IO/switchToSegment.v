260de96b2520abd97b44bd702244ffb96078792b
Nolan Gelinas <nolangelinas@gmail.com>
2023-03-30 16:23:22 -0400

    Fix addi_basic

--- IO/switchToSegment.v
+++ IO/switchToSegment.v
@@ -6,18 +6,18 @@ module SwitchToSegment(
     input [3:0] SW;
 
     wire [6:0] SEGMENT[8:0];
-    wire [31:0] currentReg;
+    wire [31:0] currentRegData;
 
-    assign currentReg = SW == 4'b0001 ? reg1 :
-                        SW == 4'b0010 ? reg2 :
-                        SW == 4'b0011 ? reg3 :
-                        SW == 4'b0100 ? reg4 :
-                        SW == 4'b0101 ? reg5 :
-                        SW == 4'b0110 ? reg6 :
-                        SW == 4'b0111 ? reg7 :
-                        SW == 4'b1000 ? reg8 :
-                        SW == 4'b1001 ? reg9 :
-                        32'd0;
+    assign currentRegData = SW == 4'b0001 ? reg1 :
+                            SW == 4'b0010 ? reg2 :
+                            SW == 4'b0011 ? reg3 :
+                            SW == 4'b0100 ? reg4 :
+                            SW == 4'b0101 ? reg5 :
+                            SW == 4'b0110 ? reg6 :
+                            SW == 4'b0111 ? reg7 :
+                            SW == 4'b1000 ? reg8 :
+                            SW == 4'b1001 ? reg9 :
+                            32'd0;
 
     assign SEGMENT[0] = 7'b1111111;
     assign SEGMENT[1] = 7'b0000110;
@@ -30,16 +30,16 @@ module SwitchToSegment(
     assign SEGMENT[8] = 7'b1111111;
     assign SEGMENT[9] = 7'b1101111;
 
-    assign SEG = currentReg == 32'd0 ? SEGMENT[0] :
-                 currentReg == 32'd1 ? SEGMENT[1] :
-                 currentReg == 32'd2 ? SEGMENT[2] :
-                 currentReg == 32'd3 ? SEGMENT[3] :
-                 currentReg == 32'd4 ? SEGMENT[4] :
-                 currentReg == 32'd5 ? SEGMENT[5] :
-                 currentReg == 32'd6 ? SEGMENT[6] :
-                 currentReg == 32'd7 ? SEGMENT[7] :
-                 currentReg == 32'd8 ? SEGMENT[8] :
-                 currentReg == 32'd9 ? SEGMENT[9] :
+    assign SEG = currentRegData == 32'd0 ? SEGMENT[0] :
+                 currentRegData == 32'd1 ? SEGMENT[1] :
+                 currentRegData == 32'd2 ? SEGMENT[2] :
+                 currentRegData == 32'd3 ? SEGMENT[3] :
+                 currentRegData == 32'd4 ? SEGMENT[4] :
+                 currentRegData == 32'd5 ? SEGMENT[5] :
+                 currentRegData == 32'd6 ? SEGMENT[6] :
+                 currentRegData == 32'd7 ? SEGMENT[7] :
+                 currentRegData == 32'd8 ? SEGMENT[8] :
+                 currentRegData == 32'd9 ? SEGMENT[9] :
                  7'b0000000;
 
 
--- update_deps.bat
+++ update_deps.bat
@@ -1,12 +1,23 @@
 @ECHO OFF
 IF EXIST .\deps.f DEL /F deps.f
-for /f %%i in ('FORFILES /S /M *.v /C "cmd /c echo @RELPATH"') do (
-    echo %%i | find "_tb.v" > nul
+
+@REM for /r %%i in (*) do ( echo %%~nxi )
+
+for /R %%F in ("*.v?") do (
+    echo %%~nxF | find "_tb.v" > nul
     if errorlevel 1 (
-        echo %%~i >> deps.f
-    ) else (
-        echo skipping testbench %%~i
+        echo %%F | find "vivado" > nul
+        if errorlevel 1 (
+            call :Sub %%~F
+        )
     )
 )
-
 ECHO .\Wrapper_tb.v >> deps.f
+
+goto :eof
+
+:Sub
+set str=%*
+set str=%str:C:\Users\nolan\Duke\ece350\ece350-32bit-cpu\=.\%
+echo.%str% >> deps.f
+
