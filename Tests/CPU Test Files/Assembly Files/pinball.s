e992c23f9c20a4fe15c6e52c28ccc3e67e9cba4c
nrgbistro <nolangelinas@gmail.com>
2023-04-15 13:34:11 -0400

    Fix register naming

--- Tests/CPU Test Files/Assembly Files/pinball.s
+++ Tests/CPU Test Files/Assembly Files/pinball.s
@@ -1,8 +1,8 @@
 # Created by Nolan Gelinas
 # 2023-09-04
 
-# $r24=$score is the game score
-# $r25=$btn is the button (0 = not pressed, 1 = b1, 2 = b2, 3 = b3, 4 = b4)
+# $score is the game score
+# $btn is the button (0 = not pressed, 1 = b1, 2 = b2, 3 = b3, 4 = b4)
 # $s0=$mult is the score multiplier
 # $t0 is the score increment value
 # $t1 is the score increment value after multiplier
@@ -12,8 +12,8 @@ addi $t0, $r0, 1
 
 # Main loop
 main:
-    # seg $0, $r24, 0
-    # bne $r3, $r0, button_press
+    # seg $0, $score, 0
+    # bne $btn, $r0, button_press
 
     j main
 
@@ -21,7 +21,7 @@ main:
 # Handles button press
 button_press:
     mul $t1, $s0, $t0
-    add $r24, $r24, $t1
+    add $score, $score, $t1
     jal reset_button
     j main
 
